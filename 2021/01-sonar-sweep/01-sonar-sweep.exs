8b9bf30823a5ed9d78cca4c54656adb04ad2f076
Justin Tan <justanman@users.noreply.github.com>
2021-12-02 23:37:38 -0600

    modify elixir solution for day 1

--- 2021/01-sonar-sweep/01-sonar-sweep.exs
+++ 2021/01-sonar-sweep/01-sonar-sweep.exs
@@ -1,22 +1,21 @@
 defmodule SonarSweep do
   def part1() do
-    File.read!("input.txt")
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.map(&String.to_integer/1)
+    read_input()
     |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.filter(fn [a, b] -> b > a end)
-    |> Enum.count()
+    |> Enum.count(fn [a, b] -> b > a end)
   end
 
   def part2() do
+    read_input()
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.count(fn [a, b] -> Enum.sum(b) > Enum.sum(a) end)
+  end
+
+  def read_input() do
     File.read!("input.txt")
     |> String.trim()
     |> String.split("\n")
     |> Enum.map(&String.to_integer/1)
-    |> Enum.chunk_every(3, 1, :discard)
-    |> Enum.chunk_every(2, 1, :discard)
-    |> Enum.filter(fn [a, b] -> Enum.sum(b) > Enum.sum(a) end)
-    |> Enum.count()
   end
 end
